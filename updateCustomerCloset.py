8aa19af4fd464f7c55f4b97d97cadbb7cd007bbc
Linda Ye <lindaye@Lindas-MacBook-Pro.local>
2019-12-26 17:57:25 -0600

    Finish functions to update customer closet after they checkout

--- updateCustomerCloset.py
+++ updateCustomerCloset.py
@@ -12,30 +12,65 @@ def getCustomerBoxOrder(lastOrderNum):
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
         rdict = response.json()
-        customersToUpdate = []
+        customers = {}
         for row in rdict['items']: #for each row of customer boxes
-            customer = {}
-            customer['stylesInBox'] = {row['items'], row['item2'], row['item3'], row['item4'], row['item5'], row['item6'], row['item7'], row['item8'], row['item9'], row['item10'], row['item11'], row['item12']}
-            customer['userEmail'] = row['customerId']
-            customer['orderNum'] = row['orderNumber']
-            customersToUpdate.append(customer)
-        print(customersToUpdate)
-        return (customersToUpdate)
+            customers[row['customerId']] = {row['items'], row['item2'], row['item3'], row['item4'], row['item5'], row['item6'], row['item7'], row['item8'], row['item9'], row['item10'], row['item11'], row['item12']}
+        print(customers)
+        return (customers)
     except HTTPError as http_err:
-        print('HTTP error occurred')  # Python 3.6
+        print('HTTP error occurred')
         return None
     except Exception as err:
-        print('Other error occurred')  # Python 3.6
+        print('Other error occurred')
         return None
     else:
         print('Success, but empty response')
-        return ([])
+        return ({})
 
 def getCustomerStyles(customerEmail):
-    pass
+    customerClosetURL = 'https://www.floatthere.com/_functions/stylesOfUser/'
+    URL = customerClosetURL + customerEmail
+    print(URL)
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()
+        rdict = response.json()
+        stylesToUpdate = {}
+        for row in rdict['items']: #for each styles row
+            stylesToUpdate[row['itemName']] = row['_id']
+        print(stylesToUpdate)
+        return(stylesToUpdate)
+    except HTTPError as http_err:
+        print('HTTP error occurred')
+        return None
+    except Exception as err:
+        print('Other error occurred')
+        return None
+    else:
+        print('Success, but empty response')
+        return ({})
 
 def updateCustomerCloset(customers):
-    pass
+    stylesURL = 'https://www.floatthere.com/_functions/stylesOfUser/'
+    for (customer,box) in customers.items(): #for each customer
+        closet = getCustomerStyles(customer)
+        boxItemsNotInCloset = box - set(closet.keys())
+        print(boxItemsNotInCloset)
+        if (len(boxItemsNotInCloset) == 0 or (len(boxItemsNotInCloset) == 1 and '' in boxItemsNotInCloset)): #All box items are in customer's closet, this is the correct customer order
+            stylesToUpdate = set(closet.keys()) - box #Items in closet but not in customer box
+            for styleName in stylesToUpdate: #For each style in closet but not in box, update Styles table
+                URL = stylesURL + customer + '/' + closet[styleName]
+                print(URL)
+                try:
+                    response = requests.put(URL)
+                    response.raise_for_status()
+                except HTTPError as http_err:
+                    print('HTTP error occurred')
+                except Exception as err:
+                    print('Other error occurred')
+                else:
+                    print('Success, updated customer style')
+                return None
 
 def main():
     # print command line arguments
