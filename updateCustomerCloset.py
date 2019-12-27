470f53b1352164dab39ca35ceadb531fd6426937
Linda Ye <lindaye@Lindas-MacBook-Pro.local>
2019-12-26 19:33:36 -0600

    Add function to update customer styles daily

--- updateCustomerCloset.py
+++ updateCustomerCloset.py
@@ -3,6 +3,30 @@
 import sys
 import requests
 from requests.exceptions import HTTPError
+from datetime import datetime, timedelta
+
+def getCustomerBoxOrderByDate(lastDate):
+    customerBoxOrderURL = 'https://www.floatthere.com/_functions/customerBoxOrderByDate/' + str(lastDate);
+    print(customerBoxOrderURL)
+    try:
+        response = requests.get(customerBoxOrderURL)
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+        rdict = response.json()
+        customers = {}
+        for row in rdict['items']: #for each row of customer boxes
+            customers[row['customerId']] = {row['items'], row['item2'], row['item3'], row['item4'], row['item5'], row['item6'], row['item7'], row['item8'], row['item9'], row['item10'], row['item11'], row['item12']}
+        print(customers)
+        return (customers)
+    except HTTPError as http_err:
+        print('HTTP error occurred')
+        return None
+    except Exception as err:
+        print('Other error occurred')
+        return None
+    else:
+        print('Success, but empty response')
+        return ({})
 
 def getCustomerBoxOrder(lastOrderNum):
     customerBoxOrderURL = 'https://www.floatthere.com/_functions/customerBoxOrder/' + str(lastOrderNum);
@@ -76,9 +100,17 @@ def main():
     # print command line arguments
     for arg in sys.argv[1:]:
         print arg
-    lastOrderNum = sys.argv[1]
-    customersOrdered = getCustomerBoxOrder(lastOrderNum)
-    updateCustomerCloset(customersOrdered)
+    if (sys.argv[1] == 'order'):
+        lastOrderNum = sys.argv[2]
+        customersOrdered = getCustomerBoxOrder(lastOrderNum)
+        updateCustomerCloset(customersOrdered)
+    elif (sys.argv[1] == 'date'):
+        if (len(sys.argv) > 2):
+            lastDate = sys.argv[2]
+        else:
+            lastDate = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+        customersOrdered = getCustomerBoxOrderByDate(lastDate)
+        updateCustomerCloset(customersOrdered)
 
 if __name__ == "__main__":
     main()
